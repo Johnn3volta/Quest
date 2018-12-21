4a8fd657b7ead0af72baa7e29c4cfcc5e2410856
johnn3volta <johnn3volta@gmail.com>
2018-12-21 16:42:14 +0300

    final html

--- source/js/main.js
+++ source/js/main.js
@@ -1,56 +1,61 @@
-$(document).ready(function(){
-  $('#header-carousel').owlCarousel({
-    items : 1,
-    singleItem : true,
-    dots : false,
-    loop : true,
-    nav : true,
-    navText : [
-      '<span class=\'header-carousel-nav header-carousel-prev\'><i class=\'fa fa-angle-left\' aria-hidden=\'true\'></i></span>',
-      '<span class=\'header-carousel-nav header-carousel-next\'><i class=\'fa fa-angle-right\' aria-hidden=\'true\'></i></span>',
-    ],
-    autoplay : true,
-    autoplayTimeout : 5000,
-  });
-
-  $('#consultation').click(function(){
-    $('.popup').show();
-  });
-
-  $('.form__close').click(function(){
-    $('.popup').hide();
-  });
-
-  inputEvents();
-
-  function inputEvents(){
-    var inp = 'form input';
-
-    $(inp).focusin(function(){
-
-      var th    = $(this),
-          label = th.parent().find('label');
-
-      label.addClass('has-content');
-
+$(document).ready(function () {
+    $('#header-carousel').owlCarousel({
+        items: 1,
+        singleItem: true,
+        dots: false,
+        loop: true,
+        nav: true,
+        navText: [
+            '<span class=\'header-carousel-nav header-carousel-prev\'><i class=\'fa fa-angle-left\' aria-hidden=\'true\'></i></span>',
+            '<span class=\'header-carousel-nav header-carousel-next\'><i class=\'fa fa-angle-right\' aria-hidden=\'true\'></i></span>',
+        ],
+        autoplay: true,
+        autoplayTimeout: 5000,
     });
-    $(inp).focusout(function(){
 
-      var th    = $(this),
-          label = th.parent().find('label');
-
-      if (th.val().trim().length === 0)
-        label.removeClass('has-content');
+    $('#consultation').click(function () {
+        $('.popup').show();
     });
-  }
 
-  $('form select').change(function(){
-    var th = $(this),
-        label = th.parent().find('label');
+    $('.form__close').click(function () {
+        $('.popup').hide();
+    });
 
-    if(!label.hasClass('has-content')){
-      label.addClass('has-content')
+    inputEvents();
+
+    function inputEvents() {
+        var inp = 'form input';
+
+        $(inp).focusin(function () {
+
+            var th = $(this),
+                label = th.parent().find('label');
+
+            label.addClass('has-content');
+
+        });
+        $(inp).focusout(function () {
+
+            var th = $(this),
+                label = th.parent().find('label');
+
+            if (th.val().trim().length === 0)
+                label.removeClass('has-content');
+        });
     }
-  })
+
+    $('form select').change(function () {
+        var th = $(this),
+            label = th.parent().find('label');
+
+        if (!label.hasClass('has-content')) {
+            label.addClass('has-content')
+        }
+    });
+
+    $('.for__order').click(function (e) {
+        e.preventDefault();
+        $('.order').toggle()
+    })
 
 });
